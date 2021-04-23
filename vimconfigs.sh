e40b2c47f9638284e3529d5fbd6ec1b202ffc69e
sohi <sohi@student.unimelb.edu.au>
2021-04-24 00:46:33 +1000

    added vimrc config

---
+++ vimconfigs.sh
@@ -0,0 +1,21 @@
+# my vimrc config
+
+syntax on
+
+set nowrap
+set tabstop=4 softtabstop=4
+set shiftwidth=4
+set expandtab
+set smartindent
+set nu
+set smartcase
+set noswapfile
+set nobackup
+set undodir=~/.vim/undodir
+set undofile
+set incsearch
+
+# Following couple of line will highlight any character from the 81th characters based on pep8 convention 
+highlight OverLength ctermbg=darkred ctermfg=white guibg=#592929 
+match OverLength /\%81v.\+/
+

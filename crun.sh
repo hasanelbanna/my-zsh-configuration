95bd2f7cd6ee3f614a8d08e5cda6e6db37a61928
sohi <sohi@student.unimelb.edu.au>
2021-05-09 20:10:44 +1000

    automated c compiling

--- crun.sh
+++ crun.sh
@@ -1,3 +1,16 @@
+# Usage:
+# No more -> clang -Wall -o helloworld helloworld.c
+# SIMPLY JUST -> crun helloworld.c
+# This script will create the output format for helloworld
+
+# Installation:
+# Open terminal and type
+#       touch crun.sh
+# Then open crun.sh with your prefer editor and copy lines 11-15 from this file and paste it to crun.sh file and save.
+# Go back to terminal and source the file by typing
+#       source crun.sh
+# Godspeed!
+
 crun() {
     input="$1"
     output="${input:0:-2}"
